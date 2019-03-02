bdaa9ee32d820d168b8803b186224bd1d25b3b03
YuTengjing <2713151713@qq.com>
2019-03-02 18:52:11 +0800

    Basci layout completed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,46 +1,39 @@
-import React, { Component } from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd'
-const { Header, Content, Footer } = Layout
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-import './Home.scss'
+import './Home.scss';
 
 class App extends Component {
-    render() {
-        const logoPath: string = require('../../assets/images/clf.jpg')
+  render() {
+    const logoPath: string = require('../../assets/images/clf.jpg');
 
-        return (
-            <Layout className="home">
-                <Header className="header">
-                    <img className="logo" src={logoPath} alt="丛林蜂工作室" />
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        style={{ lineHeight: '64px' }}
-                    >
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
+    return (
+      <Layout className="home">
+        <Header className="header">
+          <img className="logo" src={logoPath} alt="丛林蜂工作室" />
+          <span className="title">人流检测控制台</span>
+          <Menu
+            className="menu"
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="4">nav 4</Menu.Item>
+          </Menu>
+        </Header>
 
-                <Content style={{ padding: '0 50px' }}>
-                    <div
-                        style={{
-                            background: '#fff',
-                            padding: 24,
-                            minHeight: 280,
-                        }}
-                    >
-                        Content
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Create by YuTengjing
-                </Footer>
-            </Layout>
-        )
-    }
+        <Content className="home-content" />
+
+        <Footer style={{ textAlign: 'center' }}>
+          Flow detection ©2019 Create by YuTengjing
+        </Footer>
+      </Layout>
+    );
+  }
 }
 
-export default App
+export default App;
